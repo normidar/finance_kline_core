d077ef65ccdaa1e8c90e53b3dde500018aba2a10
ryu tatsu <xtatsu_ryu@serio.inc>
2026-02-10 14:15:32 +0900

    Add FKCEngine and OhlcvSeriesWrapper classes for enhanced data management
    - Introduced the `FKCEngine` class with methods to add and retrieve `OhlcvSeries` instances, improving data organization.
    - Added the `OhlcvSeriesWrapper` class to facilitate interaction with `OhlcvSeries`, allowing for interval-based selection and navigation.
    - Enhanced encapsulation and usability of the OHLCV data structure within the engine.

--- lib/src/fkc_engine.dart
+++ lib/src/fkc_engine.dart
@@ -1,5 +1,49 @@
 import 'package:finance_kline_core/finance_kline_core.dart';
 
 class FKCEngine {
+  final Interval baseInterval;
+
   final Map<Interval, OhlcvSeries> _ohlcvSeriesMap = {};
+
+  FKCEngine({
+    required this.baseInterval,
+  });
+
+  void addOhlcvSeries(Interval interval, OhlcvSeries ohlcvSeries) {
+    _ohlcvSeriesMap[interval] = ohlcvSeries;
+  }
+
+  OhlcvSeries? getOhlcvSeries(Interval interval) {
+    return _ohlcvSeriesMap[interval];
+  }
+
+  OhlcvSeriesWrapper? select(Interval interval) {
+    final ohlcvSeries = getOhlcvSeries(interval);
+    if (ohlcvSeries == null) {
+      return null;
+    }
+    return OhlcvSeriesWrapper(
+      interval: interval,
+      ohlcvSeries: ohlcvSeries,
+      engine: this,
+    );
+  }
+}
+
+class OhlcvSeriesWrapper {
+  final FKCEngine _engine;
+
+  final Interval interval;
+
+  final OhlcvSeries ohlcvSeries;
+
+  OhlcvSeriesWrapper({
+    required this.interval,
+    required this.ohlcvSeries,
+    required FKCEngine engine,
+  }) : _engine = engine;
+
+  OhlcvSeriesWrapper? jumpTo(Interval interval) {
+    return _engine.select(interval);
+  }
 }
